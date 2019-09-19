8d0fecd475fd2d1374f365813f669901cb2052c8
Gunagovindasamy <gunaanjali03@gmail.com>
2019-09-19 17:22:42 +0530

    new

--- src/SnapDeal.java
+++ src/SnapDeal.java
@@ -1,7 +1,11 @@
+import java.io.File;
 import java.util.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,16 +19,21 @@ public class SnapDeal {
 	search.sendKeys("iphonex");
 	driver.findElement(By.xpath("//span[text()='Search']")).click();
 	driver.findElement(By.xpath("(//p[contains(text(),'Apple')])[1]")).click();
-	Thread.sleep(5000);
 	Set<String> all = driver.getWindowHandles();
 	List<String> li=new ArrayList<>();
 	li.addAll(all);
 	driver.switchTo().window(li.get(1));
-	Thread.sleep(3000);
 	WebElement e1 = driver.findElement(By.id("buy-button-id"));
 	e1.click();
+	TakesScreenshot tk=(TakesScreenshot)driver;
+	File s = tk.getScreenshotAs(OutputType.FILE);
+	System.out.println(s);
+	
+	
+		
 	}
-}
+	}
+
 	
 	
 	
